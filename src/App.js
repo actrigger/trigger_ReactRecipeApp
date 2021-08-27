1929c306aad5a0ed781aec7ca7edc304295e369f
trigger <trigger@anniecannons.com>
2021-08-27 09:58:46 -0700

    recloned and updated...app working buttons are not

--- src/App.js
+++ src/App.js
@@ -12,14 +12,17 @@ const App = () => {
   const [ingredients, setIngredients] = useState([]);
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(false);
-  const ref = firebase.firestore().collection("recipes");
-  // const addModal = document.querySelector("#add-modal");
-  const editModal = document.querySelector("#edit-modal");
+  const [recipe, setRecipe] = useState({});
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const modalShow = () => setShow(true);
+  const modalClose = () => setShow(true);
 
+  const ref = firebase.firestore().collection("recipes");
+
+
+  
   function getRecipes() {
     setLoading(true);
     ref.onSnapshot((querySnapshot) => {
@@ -29,188 +32,250 @@ const App = () => {
       });
       setRecipes(items);
       setLoading(false);
+      console.log(items);
     });
   }
+
+  
+
   useEffect(() => {
     getRecipes();
   }, []);
 
-  function saveRecipes(e) {
+  function saveRecipe(e) {
     e.preventDefault();
-    const updateRecipe = async (e) => {
-      e.preventDefault();
-      await ref.collection("recipe-id").add({
-        title: editModal.title.value,
-        ingredients: editModal.ingredients.value,
-      });
-      setTitle("");
-      setIngredients("");
-    };
+    ref.collection("recipe-id").add({
+      title: handleEditModal.title.value,
+      ingredients: handleEditModal.ingredients.value,
+    });
+    setTitle("");
+    setIngredients("");
+  };
+  
 
-    const deleteRecipe = () => {
-      console.log("delete");
-      setTitle("");
-      setIngredients("");
-    };
+  function deleteRecipe(recipe) {
+    ref
+      .doc(recipe.id)
+      .delete()
+      .catch((err) => {
+        console.log(err);
+      })
+      setRecipe({});
+  }
+  
 
-    const newRecipe = async () => {
-      await ref.collection.add({
-        title: "",
-        ingredients: ingredients,
+  function addRecipe(newRecipe) {
+    ref
+      .doc(newRecipe.id)
+      .set(newRecipe)
+      .catch((err) => {
+        console.log(err);
       });
+    setTitle("");
+    setIngredients([]);
+  }
+
+  function editRecipe(editedRecipe) {
+      setLoading();
+      ref 
+        .doc(editedRecipe.id)
+        .update(editedRecipe)
+        .catch((err) => {
+          console.log(err);
+        });
       setTitle("");
       setIngredients([]);
-    };
-
-    if (Button.className === "update") {
-      updateRecipe();
-    } else if (Button.className === "delete") {
-      deleteRecipe();
-    } else {
-      newRecipe();
-    }
   }
+
+  function handleEditModal(editedObject) {
+      handleShow();
+      console.log(editedObject);
+      setLoading();
+      setRecipe(editedObject);
+      setRecipe();
+  }
+
+  const newRecipe = async () => {
+    await ref.collection.add({
+      title: "",
+      ingredients: ingredients,
+    });
+    setTitle("");
+    setIngredients([]);
+  }
+
+  const handleRecipe = (event) => {
+    setTitle(event.target.value);
+    setIngredients(event.target.value);
+  }
+
+    
 
   return (
     <div>
-      <h3 className="heading">trigger's React Recipe box</h3>
-      <p className="sub-heading">
-        Create, edit, delete your recipes. Uses session storage so that a page
-        refresh will keep stored data (but not after page close)
-      </p>
-      <p className="head-link">
-        <a href="https://www.freecodecamp.org/challenges/build-a-recipe-box">
-          https://www.freecodecamp.org/challenges/build-a-recipe-box
-        </a>
-      </p>
-      {recipes.map((recipe) => {
-        return (
-          <Accordion key={recipe.id}>
-            <Card className="main-card">
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                  {recipe.title}
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="0">
-                {ingredients.map((ingredient) => {
-                  return (
-                    <Card.Body>
-                      <div key={ingredient.id}>
-                        <li>{ingredient}</li>
-                      </div>
+      <div className="header-content">
+          <h3 className="heading">trigger's React Recipe box</h3>
+          <p className="sub-heading">
+          Create, edit, delete your recipes. Uses session storage so that a page
+          refresh will keep stored data (but not after page close)
+          </p>
+          <p className="head-link">
+            <a href="https://www.freecodecamp.org/challenges/build-a-recipe-box">
+            https://www.freecodecamp.org/challenges/build-a-recipe-box
+            </a>
+          </p>
+        </div>
 
-                      <Button
-                        className="edit"
-                        variant="info"
-                        size="sm"
-                        onClick={handleShow}
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        className="delete"
-                        variant="danger"
-                        size="sm"
-                        onClick={saveRecipes}
-                      >
-                        Delete
-                      </Button>
-                    </Card.Body>
-                  );
-                })}
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
-        );
-      })}
+      <div className="modals-container">
+          <div className="edit-modal">
+            <Modal
+              className="editRecipe"
+              show={show}
+              onHide={handleClose}
+              >
+              <Modal.Header closeButton>
+                <Modal.Title>Edit Recipe</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <form>
+                  <label>Recipe Title
+                    <input
+                      type="text"
+                      name="title"
+                      placeholder={title}
+                      value={title}
+                      onChange={handleRecipe}
+                    />
+                  </label>
+                  <label>Ingredients
+                    <input
+                      type="text"
+                      name="ingredients"
+                      placeholder={ingredients}
+                      value={ingredients}
+                      onChange={handleRecipe}
+                    />
+                  </label>
+                  <Button
+                    className="close"
+                    variant="outline-dark"
+                    onClick={handleClose}
+                    >
+                    Close
+                  </Button>
+                  <Button 
+                    className="update" 
+                    variant="primary" 
+                    onClick={saveRecipe}
+                    >
+                    Submit
+                  </Button>
+                </form>
+              </Modal.Body>
+            </Modal>
+          </div>
+          <div className="add-modal">
+            <Modal
+              className="addModal"
+              show={show}
+              onHide={modalClose}
+              >
+              <Modal.Header closeButton>
+                <Modal.Title>Add Recipe</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <label>Recipe Title
+                  <input
+                    type="text"
+                    name="title"
+                    placeholder="Enter Recipe Title"
+                    value={title}
+                    id="addTitle"
+                    onChange={handleRecipe}
+                  />
+                </label>
+                <label>Ingredients
+                  <input
+                    type="text"
+                    name="ingredients"
+                    placeholder="Enter Ingredients, seperate each with a comma"
+                    value={ingredients}
+                    id="addIngredients"
+                    onChange={handleRecipe}
+                  />
+                </label>
+                <Button variant="outline-dark" onClick={modalClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={saveRecipe}>
+                  Save Changes
+                </Button>
+                <Button variant="primary" onClick={() => addRecipe(recipe)}>
+                  Submit
+                </Button>
+              </Modal.Body>
+            </Modal>
+          </div>
+        </div>
+        
+      <div className="accordian container">
+        {recipes.map(({ title, ingredients }, i) =>  {
+          return (
+            <div>
+              <div key={i}>
+                <Accordion>
+                  <Card>
+                    <Card.Header>
+                      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        {title}
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body>
+                        {ingredients.map(({ ingredient }, j) => (
+                          <div key={j}>
+                            <li>{recipe.ingredients}</li>
+                          </div>
+                        ))}
 
-      {loading ? <h1>Loading...</h1> : null}
 
-      <Modal
-        id="edit-modal"
-        className="editModal"
-        show={show}
-        onHide={handleClose}
-      >
-        <Modal.Header editButton>
-          <Modal.Title>Edit Recipe</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <label>Recipe Title</label>
-          <input
-            type="text"
-            name="title"
-            placeholder={setTitle}
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          ></input>
-          <label>Ingredients</label>
-          <input
-            type="text"
-            name="ingredients"
-            placeholder={setIngredients}
-            value={ingredients}
-            onChange={(e) => setIngredients(e.target.value)}
-          ></input>
-          <Button
-            className="close"
-            variant="outline-dark"
-            onClick={handleClose}
-          >
-            Close
-          </Button>
-          <Button className="update" variant="primary" onClick={saveRecipes}>
-            Save Changes
-          </Button>
-        </Modal.Body>
-      </Modal>
 
-      <Button
-        className="add-button"
-        variant="primary"
-        size="sm"
-        onClick={modalShow}
-      >
-        Add
-      </Button>
-
-      <Modal
-        id="add-modal"
-        className="addModal"
-        show={show}
-        onHide={handleClose}
-      >
-        <Modal.Header addButton>
-          <Modal.Title>Add Recipe</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <label>Recipe Title</label>
-          <input
-            type="text"
-            name="title"
-            placeholder="Enter Recipe Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          ></input>
-          <label>Ingredients</label>
-          <input
-            type="text"
-            name="ingredients"
-            placeholder="Enter Ingredients, seperate each with a comma"
-            value={ingredients}
-            onChange={(e) => setIngredients(e.target.value)}
-          ></input>
-          <Button variant="outline-dark" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={saveRecipes}>
-            Save Changes
-          </Button>
-        </Modal.Body>
-      </Modal>
+                        <Button
+                          variant="info"
+                          onClick={() => handleEditModal(recipe)}
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          className="delete-button"
+                          variant="danger"
+                          onClick={deleteRecipe}
+                        >
+                          Delete
+                        </Button>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="add-button">
+        <Button
+          className="add-button"
+          variant="primary"
+          size="sm"
+          onClick={modalShow}
+        >
+          Add
+        </Button>
+      </div>     
     </div>
   );
 };
+
+
 
 export default App;
